149703bac6484ba0da8a823b728c4b0bc3bc07a0
farooq <145667687+farucq@users.noreply.github.com>
2023-09-30 16:23:39 +0530

    Add files via upload

---
+++ 30-09-2023/positiveno.py
@@ -0,0 +1,5 @@
+num=[1,5,7,-5,-7,0,8]
+for i in num:
+    
+    if i>0:
+        print("list of positive numbers:",i)
---
+++ 30-09-2023/sqr.py
@@ -0,0 +1,5 @@
+a=int(input("enter a number:"))
+sqr=a**2
+for i in range(0,a+1):
+    sqr=i*i
+    print("the sqr of num:",sqr)
